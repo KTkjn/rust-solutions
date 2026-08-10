bb4a831107a8d8511d20f930a0bbd4e2b08da212
Nap-desktop <kt.i2006mb@gmail.com>
2026-08-10 14:57:44 +0900

    desktop2026-08-10

--- 1-intro/hello/target/debug/deps/cli-41adb039c51f2abb.d
+++ 1-intro/hello/target/debug/deps/cli-41adb039c51f2abb.d
@@ -1,5 +1,5 @@
-/home/kubuntu/Documents/Code/Rust/rust-solutions/1-intro/hello/target/debug/deps/cli-41adb039c51f2abb.d: tests/cli.rs
+/home/kubuntu/Documents/Scripts/rust-solutions/1-intro/hello/target/debug/deps/cli-41adb039c51f2abb.d: tests/cli.rs
 
-/home/kubuntu/Documents/Code/Rust/rust-solutions/1-intro/hello/target/debug/deps/cli-41adb039c51f2abb: tests/cli.rs
+/home/kubuntu/Documents/Scripts/rust-solutions/1-intro/hello/target/debug/deps/cli-41adb039c51f2abb: tests/cli.rs
 
 tests/cli.rs:
--- 1-intro/hello/tests/cli.rs
+++ 1-intro/hello/tests/cli.rs
@@ -2,4 +2,6 @@ use std::process::Command;
 #[test]
 fn works(){
     let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok())
 }
