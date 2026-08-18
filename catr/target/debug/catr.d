de00a71875aff3ec10d49621c936a6a35205aa7f
Nap-desktop <kt.i2006mb@gmail.com>
2026-08-19 01:19:25 +0900

    desktop

---
+++ catr/target/debug/catr.d
@@ -0,0 +1,1 @@
+/home/kubuntu/Documents/Scripts/rust-solutions/catr/target/debug/catr: /home/kubuntu/Documents/Scripts/rust-solutions/catr/src/main.rs
---
+++ catr/target/debug/deps/catr-7f6e9cc96ffb49fe.d
@@ -0,0 +1,5 @@
+/home/kubuntu/Documents/Scripts/rust-solutions/catr/target/debug/deps/catr-7f6e9cc96ffb49fe.d: src/main.rs
+
+/home/kubuntu/Documents/Scripts/rust-solutions/catr/target/debug/deps/catr-7f6e9cc96ffb49fe: src/main.rs
+
+src/main.rs:
